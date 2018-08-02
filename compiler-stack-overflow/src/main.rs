e467d1748ac0e9e64f6d7cfa3ee3512607d242e4
DutchGhost <kasper199914@gmail.com>
2018-08-02 18:54:16 +0200

    update rustc version

---
+++ compiler-stack-overflow/src/main.rs
@@ -0,0 +1,15 @@
+#![feature(existential_type)]
+
+pub trait Bar<T> {
+    type Item;
+}
+
+existential type Foo: Bar<Foo, Item = Foo>;
+
+fn crash(x: Foo) -> Foo {
+    x
+}
+
+fn main() {
+    
+}
---
+++ existential-type-ice/src/main.rs
@@ -0,0 +1,12 @@
+#![feature(existential_type)]
+
+existential type Foo: Copy;
+
+// make compiler happy about using 'Foo'
+fn bar(x: Foo) -> Foo { x }
+
+fn main() {
+    
+    // make compiler happy about the types??
+    let _: Foo = unsafe { std::mem::transmute(0u8) };
+}
